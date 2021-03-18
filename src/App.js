a70c8a08d55c0f973e5d2427e33f8c41ca489350
Joel Corey <joelcorey@fastmail.com>
2021-03-18 09:30:42 -0700

    set up initial fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+	const [workOrders, setWorkOrders] = useState({});
+
+	useEffect(async () => {
+		fetch('​https://api.hatchways.io/assessment/work_orders')
+			.then(response => {
+				setWorkOrders(response);
+			})
+	}, [] )
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       this is header
       </header>
+			<div>{workOrders}</div>
+			
     </div>
   );
 }
